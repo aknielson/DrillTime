3f498b4517e50c1bf4a25edb7d95a02b5b5ae5d2
aknielson <aknielson@hotmail.com>
2017-03-05 10:19:28 -0800

    hmmm where are the rest of the files

--- DrillTime/DrillTime.NgOnly/app/app.js
+++ DrillTime/DrillTime.NgOnly/app/app.js
@@ -1,5 +1,33 @@
 ﻿(function () {  //use //IIFE
     "use strict";
     var app = angular.module("drillTimeNgOnly",
-        ["common.services"]);  //having the second parameter of an array means we are 'setting' the module, without the secound param, its a 'getter'
+        ['ui.router', "common.services"]);  //having the second parameter of an array means we are 'setting' the module, without the secound param, its a 'getter'
+
+    app.config(
+        [
+            "$stateProvider",
+            "$urlRouterProvider",
+            function ($stateProvider, $urlRouterProvider) {
+                $urlRouterProvider.otherwise("/");
+                $stateProvider
+                    .state("home",
+                    {
+                        url: "/",
+                        templateUrl: "app/welcomeView.html"
+                    })
+                    .state("drillList",
+                    {
+                        url: "/drills",
+                        templateUrl: "app/drills/drillListView.html",
+                        controller: "DrillListCtrl as vm"
+                    })
+                    .state("drillEdit",
+                    {
+                        url: "/drills/edit/:drillId",
+                        templateUrl: "app/drills/drillEditView.html",
+                        controller: "DrillEditCtrl as vm"
+                    });
+            }
+        ]
+    );
 }());
